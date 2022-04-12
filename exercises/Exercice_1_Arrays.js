351615273139b7fbe8823edb16063bb87e91fda0
01000001kuma <a_illera@yahoo.com>
2022-04-12 12:45:46 +0200

    Finishing: Compliting exercice

---
+++ exercises/Exercice_1_Arrays.js
@@ -0,0 +1,42 @@
+
+//1. Using `push` and `unshift`, make this array contain the numbers from zero through seven:
+
+
+let arr = [2, 3, 4];
+let arr1 = "";
+(arr.unshift(0,1) + arr.push(5,6,7));
+
+// your code here
+
+console.log(arr); // => [0, 1, 2, 3, 4, 5, 6, 7]
+
+
+//2. What is _returned_ by `push`? Before throwing this into the console, form a hypothesis about what you think the return value will be:
+
+
+let arrOf = [5, 7, 9];
+arrOf.push(6); // returns [5,7,9,6]
+
+
+//3. Change all numbers to be those numbers multiplied by two:
+
+
+let numbers = [4, 9, 7, 2, 1, 8];
+
+// your code here
+
+numbers; // => [8, 18, 14, 4, 2, 16]
+
+
+//4. Change all **odd** numbers to be those numbers multiplied by two:
+
+
+
+numbers = [4, 9, 7, 2, 1, 8];
+
+// your code here
+
+numbers; // => [4, 18, 14, 2, 2, 8]
+
+
+
